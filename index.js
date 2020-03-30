270322d0e752896f35bbbbb355120616dccc174d
nex <ngocminhluong182@gmail.com>
2020-03-30 10:48:43 +0700

    fix bugs

--- index.js
+++ index.js
@@ -1,33 +1,30 @@
 const NAMES = ['alfred', 'alfie', 'alfie'];
 
- 
-
 function solve(names) {
     let existingNames = [];
     let count = {};
     let result = [];
-    for(let name of names) {
-        s = "";
-        count[name] = (count[name]||0) + 1;
-        for(let char of name) {
-            s+=char;
-            if(!existingNames.some(name => name.includes(s))) {
+    for (let name of names) {
+        count[name] = (count[name] || 0) + 1;
+        let s = "";
+        for (let char of name) {
+            s += char;
+            if (!existingNames.some(name => name.slice(0, s.length) == s)) {
                 result.push(s);
                 break;
-            }                
+            }
         }
-        if(s===name) result.push([name, count[name]].join(" "));
+        if (s === name && s.length > 1) result.push([name, count[name]].join(" "));
         existingNames.push(name);
     }
     return result;
 }
 
- 
+
 
 function main() {
-  const res = solve(NAMES);
-  console.log((res || []).join('\n'));
+    const res = solve(NAMES);
+    console.log((res || []).join('\n'));
 }
 
 main();
-
