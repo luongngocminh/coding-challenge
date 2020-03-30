da68beedf045fa5ebaf8a4c9a1f23ae8d7e3835c
nex <ngocminhluong182@gmail.com>
2020-03-30 12:05:49 +0700

    fix more bugs

--- index.js
+++ index.js
@@ -1,4 +1,104 @@
-const NAMES = ['alfred', 'alfie', 'alfie', 'alfiee ', 'zzz'];
+const NAMES = `ghfd
+gcjbch
+j
+gagc
+bihjcac
+hfjc
+ijh
+gbcj
+bjeh
+efadgbagd
+agb
+fehgfg
+dhefcfehee
+ahig
+iedbejcag
+jcggejfae
+hbhchccgb
+g
+fj
+jajbh
+bbfjhhgh
+gfgd
+eibcjdjaii
+ajgdif
+bbfjiei
+ad
+e
+eehgd
+hf
+gcbhifhebi
+jhfdii
+bijg
+dddig
+e
+iijhhgeda
+ajcf
+afjeg
+ccehhfeibc
+jdgiacbaf
+ba
+fagegcfig
+ieh
+eag
+jja
+bdbbadead
+bjgjddiafg
+eaaegcg
+fgjihcic
+jgachgbdcb
+jjbejb
+h
+ihaeia
+cjgba
+cccaf
+cjacid
+aeajbjgcf
+ejjdg
+f
+c
+edfbhedbeg
+eccgebciij
+iii
+ididddi
+e
+gijagihj
+d
+dhdc
+fcgfihj
+aebaeih
+i
+cehjdjc
+dabhijbfe
+cfhejjefjd
+haibjj
+icfdejac
+b
+gcbcahdbbj
+f
+hheageh
+ificg
+g
+egcijga
+ababdhhc
+efcacja
+jefbadhiig
+e
+hgajhfj
+ifddidhj
+hihe
+ja
+iifiedhbdi
+j
+jjdcedhb
+cac
+hf
+he
+hb
+bh
+e
+i`;
+let test = NAMES.split("\n");
 
 function solve(names) {
     let existingNames = [];
@@ -9,13 +109,18 @@ function solve(names) {
         let s = "";
         for (let char of name) {
             s += char;
+            let  = true
             if (!existingNames.some(name => name.slice(0, s.length) == s)) {
                 result.push(s);
                 break;
             }
         }
 
-        if (s === name && count[name] > 1) result.push([name, count[name]].join(" "));
+        if (existingNames.some(existingName => (existingName.slice(0, name.length) == name)) && !existingNames.some(exName => (name == exName))) {
+            result.push(name);
+        } 
+            
+        if (existingNames.some(existingName => existingName == name)) result.push([name, count[name]].join(" "));
         existingNames.push(name);
     }
     return result;
@@ -24,7 +129,7 @@ function solve(names) {
 
 
 function main() {
-    const res = solve(NAMES);
+    const res = solve(test);
     console.log((res || []).join('\n'));
 }
 
